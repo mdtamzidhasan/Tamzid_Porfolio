2173a37b3d3ec9c9de81eb2523c9725a63035ac4
mdtamzidhasan <tamzidhasan2020@gmail.com>
2026-03-14 22:49:01 +0600

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Seeder\AdminSeeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(AdminSeeder::class);
         // User::factory(10)->create();
 
         User::factory()->create([
